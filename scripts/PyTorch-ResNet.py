b851c2c779819348dfa341c58a3d3983ca39b973
Gedamu Tinsae <tinsaegedamu@gmail.com>
2026-02-15 23:13:19 +0000

    Switch model from ResNet-50 to ResNet-18 in PyTorch script

--- scripts/PyTorch-ResNet.py
+++ scripts/PyTorch-ResNet.py
@@ -2,7 +2,9 @@ import torch
 import torchvision.models as models
 
 # Call the ResNet-50 model from torchvision
-model = models.resnet50(weights='IMAGENET1K_V1')
+
+# resnet18 vs resnet50
+model = models.resnet18(weights='IMAGENET1K_V1')
 
 # Print the network to see the detailed architecture
 print(model)
